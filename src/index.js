655dced4ec03b7e92acb80c7210962aeebf1b4b4
mpyw <ryosuke_i_628@yahoo.co.jp>
2017-06-17 05:00:59 +0900

    Add JSDoc

--- src/index.js
+++ src/index.js
@@ -1,6 +1,45 @@
 import { createActions, handleActions } from 'redux-actions'
 import { takeEvery } from 'redux-saga/effects'
 
+/**
+ * Create a ducks module.
+ *
+ *
+ * @param {string} [moduleName] - Module name used for action type prefixes.
+ *                                  e.g. "example" will produce an action named "example/doSomething".
+ *
+ * @param {Object} [definitions] - Pairs of action type and handler maps.
+ *
+ *   { ACTION_TYPE: { creator, reducer, worker } }
+ *
+ *     creator {?(Function|Function[])} - Payload creator that can receive multiple arguments.
+ *                                        You can pass meta creator as second item of array.
+ *                                          default: (payload) => payload
+ *
+ *     reducer {?Function}              - Switchless reducer that corresponds to specific action type.
+ *                                        Note that state is shared among all reducers in a specific module.
+ *                                          default: (state) => state
+ *
+ *     worker  {?(Function|Object)}     - Generator function:
+ *                                          Generator function is automatically delegated to takeEvery().
+ *                                          Returned action is automatically dispatched by put().
+ *                                      - Saga effect object:
+ *                                          You can manually wrap generator functions with any saga effects.
+ *
+ * @param {*} [defaultState] - Default state object.
+ *
+ * @return {Object} Returns a ducks module object.
+ *
+ *  { reducer, sagas, doSomething, doSomethingSuccess, doSomethingFailure }
+ *
+ *    reducer {Function}  - Composed reducer.
+ *    sagas   {?Object[]} - Array of sagas or undefined.
+ *
+ *    doSomething        {?Function} - Action creators based on naming convention:
+ *    doSomethingSuccess               SCREAMING_SNAKE_CASE to camelCase.
+ *    doSomethingFailure
+ *    ...
+ */
 export const createModule = (moduleName, definitions, defaultState) => {
 
   const identityActions = []
