e15085cb4a8b2c6238a96ee906e9acf59a01d92f
mpyw <ryosuke_i_628@yahoo.co.jp>
2017-06-19 05:22:55 +0900

    Breaking change! Resolved many problems

--- src/index.js
+++ src/index.js
@@ -1,64 +1,58 @@
 import { createActions, handleActions } from 'redux-actions'
-import { takeEvery, takeLatest, put } from 'redux-saga/effects'
-
-const helpers = {
-  takeEvery,
-  takeLatest,
-}
-
-const defaultOptions = {
-  useHelper: true,
-  helper: 'takeEvery',
-}
+import { takeEvery, takeLatest, throttle, fork, spawn, put } from 'redux-saga/effects'
 
 export const createModule = (
   moduleName,
   definitions,
   defaultState,
 ) => {
+
   const identityActions = []
   const actionMap = {}
   const reducerMap = {}
   const sagas = []
 
   for (const [type, definition] of Object.entries(definitions)) {
+
     const actionType = `${moduleName}/${type}`
     const {
       creator,
       reducer,
       saga,
       onError,
-      options: {
-        helper = defaultOptions.helper,
-        useHelper = defaultOptions.useHelper,
-      } = defaultOptions,
     } = typeof definition === 'function' ? { reducer: definition } : definition
 
     creator ? actionMap[actionType] = creator : identityActions.push(actionType)
     reducer && (reducerMap[actionType] = reducer)
 
-    saga && sagas.push(
-      useHelper
-        // We wrap it into an auto-puttable generator.
-        ? helpers[helper](actionType, function* (action) {
-          try {
-            const result = yield* saga(action)
-            if (result !== undefined) {
-              yield put(result)
-            }
-          } catch (e) {
-            if (!onError) {
-              throw e
-            }
+    if (isGeneratorFunction(saga)) {
 
-            const result = isGeneratorFunction(onError) ? (yield* onError(e)) : onError(e)
-            if (result !== undefined) {
-              yield put(result)
-            }
-          }
-        })
-        : saga
-    )
+      sagas.push(takeEvery(actionType, enhanceThunk(onError)(saga)))
+
+    } else if (isNormalFunction(saga)) {
+
+      const enhance = enhanceThunk(onError)
+      const returnValue = saga({
+        TYPE: actionType,
+        ...Object
+          .entries(enhancibleForkEffectThunks)
+          .reduce((prev, [key, value]) => ({ ...prev, [key]: value(enhance) }), {}),
+        enhance,
+      })
+      if (isGeneratorFunction(returnValue)) {
+        sagas.push(fork(returnValue))
+      } else if (isForkEffect(returnValue)) {
+        sagas.push(returnValue)
+      } else {
+        throw new Error('Invalid saga: Non-generator function must return generator function or redux-saga FORK effect.')
+      }
+
+    } else if (saga) {
+
+      throw new Error('Invalid saga: Saga must be specified as generator function or thunk that returns redux-saga FORK effect.')
+
+    }
+
   }
 
   return {
@@ -74,11 +68,48 @@ export const createModule = (
 }
 
 const isGenerator = obj => {
-  return 'function' == typeof obj.next && 'function' == typeof obj.throw
+  return obj && 'function' == typeof obj.next && 'function' == typeof obj.throw
 }
 
 const isGeneratorFunction = obj => {
-  if (!obj.constructor) return false
+  if (!obj || !obj.constructor) return false
   if ('GeneratorFunction' === obj.constructor.name || 'GeneratorFunction' === obj.constructor.displayName) return true
   return isGenerator(obj.constructor.prototype)
+}
+
+const isNormalFunction = obj => {
+  if (!obj || !obj.constructor) return false
+  return 'Function' === obj.constructor.name || 'Function' === obj.constructor.displayName
+}
+
+const isForkEffect = obj => {
+  return obj && obj['@@redux-saga/IO'] && obj['FORK']
+}
+
+const enhancibleForkEffectThunks = {
+  takeEvery: enhance => (patternOrChannel, worker, ...args) => takeEvery(patternOrChannel, enhance(worker), ...args),
+  takeLatest: enhance => (patternOrChannel, worker, ...args) => takeLatest(patternOrChannel, enhance(worker), ...args),
+  throttle: enhance => (ms, pattern, worker, ...args) => throttle(ms, pattern, enhance(worker), ...args),
+  fork: enhance => (fn, ...args) => fork(enhance(fn), ...args),
+  spawn: enhance => (fn, ...args) => spawn(enhance(fn), ...args),
+}
+
+const enhanceThunk = onError => saga => function* (...args) {
+  if (!isGeneratorFunction(saga)) {
+    throw new Error('Enhanced target must be generator function.')
+  }
+  try {
+    const result = yield* saga(...args)
+    if (result !== undefined) {
+      yield put(result)
+    }
+  } catch (e) {
+    if (!onError) {
+      throw e
+    }
+    const result = isGeneratorFunction(onError) ? (yield* onError(e)) : onError(e)
+    if (result !== undefined) {
+      yield put(result)
+    }
+  }
 }
