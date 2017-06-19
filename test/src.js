d4d6ba0bf99aef482802169c61779b1243ec1bcf
mpyw <ryosuke_i_628@yahoo.co.jp>
2017-06-20 07:06:56 +0900

    Add test of flattenSagas()

---
+++ test/src.js
@@ -0,0 +1,42 @@
+import test from 'tape'
+import { takeEvery, takeLatest, throttle, fork, spawn } from 'redux-saga/effects'
+import { flattenSagas } from '../src'
+
+test('it should flatten nested objects and arrays into a flat array of redux-saga effects', assert => {
+
+  const expected = [
+    takeEvery('A', function* () { }),
+    takeLatest('B', function* () { }),
+    throttle(100, 'C', function* () { }),
+    fork(function* () { }),
+    spawn(function* () { }),
+  ]
+
+  const actual = flattenSagas(
+    {
+      a: {
+        b: expected[0],
+        c: {
+          d: expected[1],
+          e: 'foo',
+          f: [],
+        },
+      },
+      g: expected[2],
+    },
+    [
+      {
+        h: expected[3],
+      },
+      'bar',
+      {
+        i: [
+          expected[4],
+        ],
+      },
+    ],
+  )
+
+  assert.deepEqual(actual, expected)
+  assert.end()
+})
