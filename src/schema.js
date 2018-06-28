f4770a2c49e5b7550f7ecc782569b1014b893abb
mpyw <ryosuke_i_628@yahoo.co.jp>
2018-06-28 20:13:00 +0900

    Fix: Allow webpack v4 "Module" import

--- src/schema.js
+++ src/schema.js
@@ -3,8 +3,16 @@ import yup from 'yup'
 const requiredFn = yup.mixed().test('required-function', '${path} is not a function', v => typeof v === 'function')
 const nonRequiredFn = yup.mixed().test('non-required-function', '${path} is not a function', v => v === undefined || typeof v === 'function')
 
-const schema = yup.object().shape({
-  effects: yup.object().required().when('defaultEffect', (defaultEffect, schema) => schema.shape({
+const effects = yup
+  .object()
+  .transform((currentValue, originalValue) =>
+    // in webpack v4, wildcard import type is not "Object" but "Module"
+    originalValue && originalValue[Symbol.toStringTag] === 'Module'
+      ? Object.create(null, originalValue)
+      : currentValue
+  )
+  .required()
+  .when('defaultEffect', (defaultEffect, schema) => schema.shape({
     put: requiredFn,
     fork: requiredFn,
     spawn: nonRequiredFn,
@@ -12,8 +20,13 @@ const schema = yup.object().shape({
     takeEvery: defaultEffect === 'takeEvery' ? requiredFn : nonRequiredFn,
     takeLeading: defaultEffect === 'takeLeading' ? requiredFn : nonRequiredFn,
     takeLatest: defaultEffect === 'takeLatest' ? requiredFn : nonRequiredFn,
-  })),
-  defaultEffect: yup.string().oneOf(['takeEvery', 'takeLeading', 'takeLatest']).default('takeEvery'),
-})
+  }))
+
+const defaultEffect = yup
+  .string()
+  .oneOf(['takeEvery', 'takeLeading', 'takeLatest'])
+  .default('takeEvery')
+
+const schema = yup.object().shape({ effects, defaultEffect })
 
 export default schema
